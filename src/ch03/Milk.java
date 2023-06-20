e4b2ab6f8d46ca725e04ab7d0cb47d1f236d0b77
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-06-20 14:52:38 +0900

    Decorator Pattern

---
+++ src/ch03/Milk.java
@@ -0,0 +1,17 @@
+package ch03;
+
+public class Milk extends CondimentDecorator {
+    public Milk(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", 우유";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+}
